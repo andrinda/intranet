c85931889dc8b631c11e8e1e2e1a719498b1dd35
andrinda <andri.drmawan@gmail.com>
2020-03-06 15:35:49 +0700

    test

---
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        <a href="artotelgroup.com"></a><b>Version 1.1</b></a>. 
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020 <a href="artotelgroup.com">Arotelgroup.com</a>.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="#">ICT</a>. </a>
+</footer>
